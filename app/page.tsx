4379d714fc77cb080ba3ccfadd19718f96735474
solvman <solvman@gmail.com>
2024-07-16 22:12:16 -0500

    Add html layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { Result } from "postcss";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -8,9 +8,74 @@ export default function Home() {
       </h1>
       <article>
         {/* Calculator */}
-        <section></section>
+        <section>
+          <header>
+            <h2>Mortgage Calculator</h2>
+            <button>Clear All</button>
+          </header>
+          <form>
+            <div>
+              <label htmlFor="amount">Mortgage Amount</label>
+              <input type="number" id="amount" />
+            </div>
+            <div>
+              <label htmlFor="term">Mortgage Term</label>
+              <input type="number" id="term" />
+            </div>
+            <div>
+              <label htmlFor="interest">Interest Rate</label>
+              <input type="number" id="interest" />
+            </div>
+            <div>
+              <div>
+                <label htmlFor="repayment">Repayment</label>
+                <input type="radio" id="repayment" name="mortgage-type" />
+              </div>
+              <div>
+                <label htmlFor="interest-only">Interest Only</label>
+                <input type="radio" id="interest-only" name="mortgage-type" />
+              </div>
+            </div>
+            <button>
+              <Image
+                src="/images/icon-calculator.svg"
+                width={24}
+                height={24}
+                alt="Calculator Icon"
+              />
+              Calculate Repayments
+            </button>
+          </form>
+        </section>
         {/* Results */}
-        <section></section>
+        <section>
+          {/* Empty Results */}
+          <Image
+            src="/images/illustration-empty.svg"
+            alt="Abstract calculator"
+            width={192}
+            height={192}
+          />
+          <h2>Results shown here</h2>
+          <p>
+            Complete the form and click “calculate repayments” to see what your
+            monthly repayments would be.
+          </p>
+          {/* Results */}
+          <h2>Your results</h2>
+          <p>
+            Your results are shown below based on the information you provided.
+            To adjust the results, edit the form and click “calculate
+            repayments” again.
+          </p>
+          <div>
+            <p>Your monthly repayments</p>
+            <p>$1234</p>
+            {/* Divider */}
+            <p>Total you`&apos;`ll repay over the term</p>
+            <p>$123456</p>
+          </div>
+        </section>
       </article>
     </main>
   );
